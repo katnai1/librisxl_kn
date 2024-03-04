301aaa2275e8fb24c3eb8cb542144910315425e2
katnai1 <104574704+katnai1@users.noreply.github.com>
2024-03-04 16:37:40 +0100

    Revert "Add files via upload (#2)"
    This reverts commit 5ce838c8ed7fa642cf7eaf912eae07046a042077.

--- whelktool/scripts/syslib/2024/resaveUmea.groovy
+++
@@ -1,7 +0,0 @@
-// MUST be run with --allow-loud
-
-File holdsToResave = new File(scriptDir, "umea_export_test.txt")
-
-selectByIds( holdsToResave.readLines() ) { hold ->
-    hold.scheduleSave(loud: true)
-}
